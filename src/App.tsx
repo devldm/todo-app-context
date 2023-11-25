c4408a8f4fea547bb359a6fcd6e1e3ced96ca22c
liam <39243060+devldm@users.noreply.github.com>
2023-11-25 11:02:41 +0900

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="w-full max-h-screen flex flex-row items-center justify-center ">
-      <div data-testid="app" className="w-[40%] max-h-screen">
+      <div data-testid="app" className="lg:w-[30%] max-h-screen">
         <TodoInput />
         <TodoList />
       </div>
